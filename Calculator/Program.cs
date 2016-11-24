378d759f93228a0cc92aa222c69d2d75e05b6b7a
bluebeel <saikouah@gmail.com>
2016-11-24 11:42:47 +0100

    Add .dll

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -10,12 +10,33 @@ namespace Calculator
 		public static void Main(string[] args)
 		{
 			char[] delimiterChars = {' '};
+			Dictionary<string, Type> commands = new Dictionary<string, Type>();
 			List<double> arr = new List<double>();
+
+			foreach (string sFileName in System.IO.Directory.GetFiles("../../dll"))
+			{
+				if (System.IO.Path.GetExtension(sFileName) == ".dll")
+				{
+					Assembly computer = Assembly.LoadFrom(sFileName);
+					string nameSpace = "BasicCommand";
+					var q = from t in computer.GetTypes()
+							where t.IsClass && t.Namespace == nameSpace
+							select t;
+					foreach (var command in q) {
+						if (command.Name == "Command")
+						{
+							continue;
+						}
+						else {
+							commands.Add(command.Name, command);
+						}
+					}
+				}
+			}
 		
 			Console.WriteLine("===  Bienvenue  dans  Calculator  3.1  ===");
 			while (true)
 			{
-				Assembly computer = Assembly.LoadFrom("../../BasicCommand.dll");
 				Console.Write(">>>  ");
 				string[] arguments = Console.ReadLine().Split(delimiterChars);
 				try
@@ -26,32 +47,32 @@ namespace Calculator
 				{
 					Console.WriteLine(new InvalidArgumentException("letters", "numbers"));
 				}
-				try
-				{
-					if (arguments[0] == "Exit" || arguments[0] == "exit")
+				if (arguments[0] == "Exit" || arguments[0] == "exit")
 					{
 						Environment.Exit(0);
 					}
-					else
+				else
+				{
+					try
 					{
-						Type commandType = computer.GetType("BasicCommand" + "." + arguments[0]);
-
 						// Activator.CreateInstance calls the parameterless constructor
 						// of the given Type to create an instace.
-						var command = Activator.CreateInstance(commandType, arr);
+						var command = Activator.CreateInstance(commands[arguments[0]], arr);
 						var method = command.GetType().GetMethod("Execute");
 
 						// We call the method we found with no parameters
 						Console.WriteLine("Result: " + method.Invoke(command, null));
 					}
-				}
-				catch (ArgumentNullException)
-				{
-					// If the command was not found, throw an exception 
-					//throw new CommandNotFoundException(commandName);
-					Console.WriteLine("Command not found");
+					catch (ArgumentNullException)
+					{
+						// If the command was not found, throw an exception 
+						//throw new CommandNotFoundException(commandName);
+						Console.WriteLine("Command not found : " + arguments[0]);
+					}
+						
 				}
 			}
+				
 		}
 	}
 }
